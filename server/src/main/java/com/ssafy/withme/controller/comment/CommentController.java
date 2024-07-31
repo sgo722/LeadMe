95482c4b4593d4de011d88f99bd97ad2aaaa68d5
sgo722@naver.com <sgo722@naver.com>
2024-07-31 14:30:48 +0900

    feat : @CurrentUser Comment 적용

--- server/src/main/java/com/ssafy/withme/controller/comment/CommentController.java
+++ server/src/main/java/com/ssafy/withme/controller/comment/CommentController.java
@@ -4,6 +4,8 @@ import com.ssafy.withme.controller.comment.request.CommentCreateRequest;
 import com.ssafy.withme.controller.comment.request.CommentDeleteRequest;
 import com.ssafy.withme.controller.comment.request.CommentUpdateRequest;
 import com.ssafy.withme.domain.comment.Comment;
+import com.ssafy.withme.domain.user.User;
+import com.ssafy.withme.global.annotation.CurrentUser;
 import com.ssafy.withme.global.response.SuccessResponse;
 import com.ssafy.withme.service.comment.CommentService;
 import com.ssafy.withme.service.comment.response.CommentCreateResponse;
@@ -36,20 +38,26 @@ public class CommentController {
 
     // 댓글을 등록한다.
     @PostMapping("/api/v1/comment")
-    public SuccessResponse<CommentCreateResponse> createComment(@RequestBody CommentCreateRequest request){
-        return SuccessResponse.of(commentService.create(request));
+    public SuccessResponse<CommentCreateResponse> createComment(
+            @CurrentUser User user,
+            @RequestBody CommentCreateRequest request){
+        return SuccessResponse.of(commentService.create(user, request));
     }
 
     // 댓글을 삭제한다.
     @DeleteMapping("/api/v1/comment")
-    public SuccessResponse<CommentDeleteResponse> deleteComment(@RequestBody CommentDeleteRequest request){
-        return SuccessResponse.of(commentService.delete(request));
+    public SuccessResponse<CommentDeleteResponse> deleteComment(
+            @CurrentUser User user,
+            @RequestBody CommentDeleteRequest request){
+        return SuccessResponse.of(commentService.delete(user, request));
     }
 
     // 댓글을 수정한다.
     @PutMapping("/api/v1/comment")
-    public SuccessResponse<CommentUpdateResponse> updateComment(@RequestBody CommentUpdateRequest request){
-        return SuccessResponse.of(commentService.update(request));
+    public SuccessResponse<CommentUpdateResponse> updateComment(
+            @CurrentUser User user,
+            @RequestBody CommentUpdateRequest request){
+        return SuccessResponse.of(commentService.update(user, request));
     }
 
 }
