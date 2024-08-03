4b43417fb391e113069306a5c6a1224e4e7f3583
sgo722 <sgo722@naver.com>
2024-08-03 16:34:21 +0900

    feat : 유저영상 저장 할 때, 형식 무관하게 mp4 파일로 변환 기능 및 유저 뒤집어서 저장하는 기능

--- leadme/main.py
+++ leadme/main.py
@@ -5,15 +5,19 @@ import os
 import uuid
 import config
 import ray
+import cv2
 import time
 import logging
+from pathlib import Path
 import asyncio
-
+from moviepy.editor import VideoFileClip
 from video_processor import download_video, process_video, process_video_user
 
 # 로깅 설정
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+print(Path(__file__).resolve())
 
 app = FastAPI()
 
@@ -60,14 +64,52 @@ async def saveVideDataByUserFile(videoFile: UploadFile = File(...)):
     start_time = time.time()
     unique_id = str(uuid.uuid4())
 
+    
     os.makedirs(TEMP_DIRECTORY, exist_ok=True)
-    temp_video_path = os.path.join(TEMP_DIRECTORY, f"{unique_id}.mp4")
+    original_video_path = os.path.join(TEMP_DIRECTORY, f"{unique_id}_{videoFile.filename}")
+    flipped_temp_video_path = os.path.join(TEMP_DIRECTORY, f"{unique_id}_flipped_temp.avi")
+    final_video_path = os.path.join(TEMP_DIRECTORY, f"{unique_id}.mp4")
 
-    with open(temp_video_path, "wb") as buffer:
+    # 파일을 TEMP_DIRECTORY에 원래 이름으로 저장
+    download_start = time.time()
+    with open(original_video_path, "wb") as buffer:
         shutil.copyfileobj(videoFile.file, buffer)
+    download_end = time.time()
+
+    # 비디오 파일을 수평으로 뒤집고 임시 파일로 저장
+    try:
+        flip_start = time.time()
+        # 원본 비디오를 읽어와서 뒤집기
+        cap = cv2.VideoCapture(original_video_path)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(flipped_temp_video_path, fourcc, cap.get(cv2.CAP_PROP_FPS), 
+                            (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            flipped_frame = cv2.flip(frame, 1)  # 수평으로 뒤집기
+            out.write(flipped_frame)
+
+        cap.release()
+        out.release()
+        flip_end = time.time()
+
+        convert_start = time.time()
+        # 뒤집힌 비디오 파일을 mp4로 변환
+        clip = VideoFileClip(flipped_temp_video_path)
+        clip.write_videofile(final_video_path, codec="libx264")
+        convert_end = time.time()
+    except Exception as e:
+        return {"error": str(e)}
+    
+    # 원본 비디오 파일과 임시 파일 삭제
+    os.remove(original_video_path)
+    os.remove(flipped_temp_video_path)
 
     # 비디오 처리 실행 및 결과 대기
-    keypoints = await asyncio.to_thread(lambda: ray.get(ray_process_video_user.remote(temp_video_path)))
+    keypoints = await asyncio.to_thread(lambda: ray.get(ray_process_video_user.remote(final_video_path)))
 
     total_time = time.time() - start_time
     logger.info(f"userFile API - UUID: {unique_id}, Total Time: {total_time:.4f} seconds")
