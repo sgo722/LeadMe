477c5d490eaaf7c280f0f63e7d81a57e8158ea6c
limjunhee <limjunhee4575@gmail.com>
2024-08-05 17:49:07 +0900

    배틀 방 목록 퍼블리싱

---
+++ client/src/features/battle/CreateRoomModal.tsx
@@ -0,0 +1,155 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface CreateRoomModalProps {
+  onClose: () => void;
+}
+
+export const CreateRoomModal: React.FC<CreateRoomModalProps> = ({
+  onClose,
+}) => {
+  const [isPrivate, setIsPrivate] = useState<boolean>(false); // private, public 상태
+  const [password, setPassword] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+  return (
+    <ModalOverlay onClick={handleOverlayClick}>
+      <ModalContent>
+        <ModalHeader>
+          <h2>Battle</h2>
+          <CloseButton onClick={onClose}>✕</CloseButton>
+        </ModalHeader>
+        <InputField
+          type="text"
+          placeholder="방 제목을 입력하세요"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <PrivateToggle>
+          <input
+            type="checkbox"
+            checked={isPrivate}
+            onChange={() => setIsPrivate(!isPrivate)}
+          />
+          <span>private</span>
+        </PrivateToggle>
+        {isPrivate && (
+          <InputField
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        )}
+        <StartButton>start</StartButton>
+      </ModalContent>
+    </ModalOverlay>
+  );
+};
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.65);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
+
+const ModalContent = styled.div`
+  padding: 20px;
+  width: 350px;
+  position: relative;
+  border-radius: 10px;
+  border: 3px rgba(223, 223, 223, 0.4);
+  background: rgba(252, 252, 252, 0.85);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(10px);
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  color: #ee5050;
+  font-family: Rajdhani;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 600;
+`;
+
+const CloseButton = styled.button`
+  background: none;
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  color: #ee5050;
+`;
+
+const InputField = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 4px;
+  background: #fff;
+  border: none;
+  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const PrivateToggle = styled.label`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+
+  input[type="checkbox"] {
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 4px;
+    background: #fff;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    margin-right: 8px;
+    cursor: pointer;
+    position: relative;
+
+    &:checked {
+      background: #ee5050;
+    }
+
+    &:checked::after {
+      content: "✓";
+      color: white;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-size: 13px;
+    }
+  }
+`;
+
+const StartButton = styled.button`
+  width: 100%;
+  border: none;
+  border-radius: 4px;
+  background: #f3f3f3;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+  font-size: 24px;
+  font-weight: 500;
+  color: #ee5050;
+  padding: 3px 0px;
+`;
--- client/src/pages/Battle.tsx
+++ client/src/pages/Battle.tsx
@@ -1,9 +1,62 @@
 import Header from "components/Header";
 import styled from "styled-components";
+import { useState } from "react";
 import { SearchBar } from "components/SearchBar";
 import { MdLock } from "react-icons/md";
+import { CreateRoomModal } from "features/battle/CreateRoomModal";
+import { InputPasswordModal } from "features/battle/InputPasswordModal";
 
-const Battle = () => {
+export const Battle: React.FC = () => {
+  const [showCreateRoomModal, setShowCreateRoomModal] =
+    useState<boolean>(false);
+  const [showInputPasswordModal, setShowInputPasswordModal] =
+    useState<boolean>(false); // 비밀번호 입력 모달 표시 여부
+  const [selectedRoom, setSelectedRoom] = useState<any>(null);
+
+  const handleEnterRoom = (room: any) => {
+    // 만약 비번방이면
+    if (room.isLocked) {
+      // selectedRoom에 선택한 방 객체 넣고
+      setSelectedRoom(room);
+      // 비밀번호 입력 모달창 띄우기
+      setShowInputPasswordModal(true);
+    } else {
+      // 여기에 공개방 입장하는 api 쓰기
+      console.log("비번방 아님");
+    }
+  };
+
+  const handlePasswordEnter = (password: string) => {
+    // 비번입력모달 닫기
+    // 여기에 비번방 입장하는 api 쓰기
+    console.log(password);
+    setShowInputPasswordModal(false);
+  };
+
+  const dummyRooms = [
+    {
+      id: 1,
+      userId: "user1",
+      title: "에스파 챌린지 같이 추실 분",
+      date: "2024-07-30 17:34",
+      isLocked: true,
+    },
+    {
+      id: 2,
+      userId: "user2",
+      title: "카리나 챌린지 같이 할 사람",
+      date: "2024-07-30 18:15",
+      isLocked: false,
+    },
+    {
+      id: 3,
+      userId: "user3",
+      title: "블랙핑크 댄스 배우실 분",
+      date: "2024-07-30 19:22",
+      isLocked: true,
+    },
+  ];
+
   return (
     <>
       <Header />
@@ -11,23 +64,44 @@ const Battle = () => {
         <MainSection>
           <SearchBar width={560} icon />
           <RoomContainer>
-            <Room>
-              <Profile></Profile>
-              <div>
-                <div>
-                  <div>userId</div>
-                  <div>에스파 챌린지 같이 추실 분</div>
-                </div>
-                <div>2024-07-30 17:34</div>
-              </div>
-              <div>
-                <MdLock />
-                <div>join</div>
-              </div>
-            </Room>
+            {dummyRooms.map((room) => (
+              <Room key={room.id}>
+                <RoomTop>
+                  <div>{room.userId}</div>
+                  <div>{room.isLocked && <MdLock />}</div>
+                </RoomTop>
+                <RoomMid>{room.title}</RoomMid>
+                <RoomBottom>
+                  <RoomCreatedAt>{room.date}</RoomCreatedAt>
+                  <EnterButton onClick={() => handleEnterRoom(room)}>
+                    enter
+                  </EnterButton>
+                </RoomBottom>
+              </Room>
+            ))}
           </RoomContainer>
         </MainSection>
+        <Footer>
+          <PaginationContainer>
+            <div>1,2,3</div>
+          </PaginationContainer>
+          <CreateRoomButtonContainer>
+            <CreateRoomButton onClick={() => setShowCreateRoomModal(true)}>
+              방만들기
+            </CreateRoomButton>
+          </CreateRoomButtonContainer>
+        </Footer>
       </Container>
+      {showCreateRoomModal && (
+        <CreateRoomModal onClose={() => setShowCreateRoomModal(false)} />
+      )}
+      {showInputPasswordModal && selectedRoom && (
+        <InputPasswordModal
+          onClose={() => setShowInputPasswordModal(false)}
+          onEnter={handlePasswordEnter}
+          roomTitle={selectedRoom.title}
+        />
+      )}
     </>
   );
 };
@@ -35,6 +109,7 @@ const Battle = () => {
 const Container = styled.div`
   min-width: 1120px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   margin: 50px auto;
@@ -58,25 +133,105 @@ const MainSection = styled.div`
 `;
 
 const RoomContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  border: 1px solid white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+  min-height: 400px;
 `;
 
 const Room = styled.div`
-  width: 46%;
-  border: 1px solid black;
   display: flex;
-  flex-direction: row;
-  padding: 20px 30px;
+  flex-direction: column;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.1);
+  height: 180px; // 고정 높이 설정
+  border-radius: 15px;
+  border: 1px #fff;
+  background: linear-gradient(
+    108deg,
+    rgba(255, 255, 255, 0.8) 0%,
+    rgba(255, 255, 255, 0.2) 100%
+  );
+  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.15);
+  backdrop-filter: blur(10px);
 `;
 
-const Profile = styled.div`
-  width: 60px;
-  height: 60px;
-  background-color: #e6e6e6;
-  border-radius: 50%;
-  margin-right: 16px;
+const RoomTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  color: #929292;
 `;
 
-export default Battle;
+const RoomMid = styled.h1`
+  margin-top: 10px;
+  margin-bottom: auto;
+  font-size: 20px;
+  color: black;
+`;
+
+const RoomBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const RoomCreatedAt = styled.div`
+  color: #c1c1c1;
+  text-align: center;
+  font-family: Inter;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 15px;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1080px;
+  gap: 20px; // 페이징과 버튼 사이의 간격
+`;
+
+const EnterButton = styled.button`
+  width: 80px;
+  height: 30px;
+  border-radius: 4px;
+  background: #f7f7f7;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
+  border: none;
+  cursor: pointer;
+  color: #ee5050;
+  text-align: center;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 23px;
+`;
+
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+const CreateRoomButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+`;
+
+const CreateRoomButton = styled.button`
+  width: 96px;
+  height: 31px;
+  border-radius: 4px;
+  background: rgba(255, 255, 255, 0.55);
+  border: none;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
+  color: #ee5050;
+  font-size: 16px;
+  font-weight: 400;
+  cursor: pointer;
+`;
