9d1753957bd64eebda189deee87e255fed3c26db
jinwoo <zvzv9808@gmail.com>
2024-08-05 10:18:26 +0900

    refactor: 유저 검색 response 수정

--- server/src/main/java/com/ssafy/withme/dto/SearchUserDto.java
+++ server/src/main/java/com/ssafy/withme/dto/SearchUserDto.java
@@ -2,10 +2,10 @@ package com.ssafy.withme.dto;
 
 import com.ssafy.withme.domain.user.User;
 
-public record SearchUserDto(Long id, String nickname) {
+public record SearchUserDto(Long id, String nickname, String profile) {
 
     public static SearchUserDto fromUser(User user) {
 
-        return new SearchUserDto(user.getId(), user.getNickname());
+        return new SearchUserDto(user.getId(), user.getNickname(), user.getProfileImg());
     }
 }
--- server/src/test/java/com/ssafy/withme/service/user/UserServiceTest.java
+++ server/src/test/java/com/ssafy/withme/service/user/UserServiceTest.java
@@ -1,10 +1,18 @@
 package com.ssafy.withme.service.user;
 
+import com.ssafy.withme.domain.user.User;
+import com.ssafy.withme.domain.user.constant.UserStatus;
 import com.ssafy.withme.repository.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserServiceTest {
@@ -14,4 +22,20 @@ class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        User user1 = User.builder()
+                .id(1L)
+                .email("test@ssafy.com")
+                .name("ssafy")
+                .userStatus(UserStatus.ACTIVE)
+                .build();
+
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user1));
+        when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user1));
+        when(userRepository.findByNameContaining("Test")).thenReturn(List.of(user1));
+    }
 }
