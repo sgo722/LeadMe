887459025228fab8baaac8232be688cf9c029208
nbo5548 <nbo5548@naver.com>
2024-08-05 09:09:23 +0900

    refactor: chat 리팩토링

--- server/src/main/java/com/ssafy/withme/dto/chat/ChatRoomGetResponse.java
+++ server/src/main/java/com/ssafy/withme/dto/chat/ChatRoomGetResponse.java
@@ -18,8 +18,6 @@ public class ChatRoomGetResponse {
     private Long partnerId;
 
     private String partnerNickname;
-//    private Long buyerId; // 구매자니까 채팅 신청한사람?
-//    private Long sellerId; // 판매자니까 채팅 신청 받은사람?
 
     private ChatMessageDto lastChatMessageDto;
 
