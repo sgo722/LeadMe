1341f550630ba139c80b15bbfb1a0f3aed05c521
jinwoo <zvzv9808@gmail.com>
2024-08-05 15:46:24 +0900

    fix: stomphandler 삭제

--- server/src/main/java/com/ssafy/withme/global/config/chat/WebSocketConfig.java
+++ server/src/main/java/com/ssafy/withme/global/config/chat/WebSocketConfig.java
@@ -1,20 +1,23 @@
 package com.ssafy.withme.global.config.chat;
 
-import com.ssafy.withme.global.handler.StompHandler;
+//import com.ssafy.withme.global.handler.StompHandler;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final StompHandler stompHandler;
+//    private final StompHandler stompHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -29,9 +32,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setAllowedOriginPatterns("*");
     }
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        /* stompHandler가 websocket 앞단에서 token을 체크할 수 있도록 인터셉터 설정*/
-        registration.interceptors(stompHandler);
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        /* stompHandler가 websocket 앞단에서 token을 체크할 수 있도록 인터셉터 설정*/
+//        registration.interceptors(stompHandler);
+//    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
