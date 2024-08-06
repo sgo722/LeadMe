03b438665bcba84adbe2eaf295024993e100647f
nbo5548 <nbo5548@naver.com>
2024-08-06 12:03:28 +0900

    fix

--- server/src/main/java/com/ssafy/withme/global/config/chat/WebSocketConfig.java
+++ server/src/main/java/com/ssafy/withme/global/config/chat/WebSocketConfig.java
@@ -29,8 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
                 .setAllowedOrigins("*")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 
 //    @Override
