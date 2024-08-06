f1707a0d236a9854267bc7240e2df9a7dd6ba302
jinwoo <zvzv9808@gmail.com>
2024-08-06 13:55:37 +0900

    fix: send dto 수정

--- server/src/main/java/com/ssafy/withme/service/chat/RedisSubscriber.java
+++ server/src/main/java/com/ssafy/withme/service/chat/RedisSubscriber.java
@@ -57,9 +57,11 @@ public class RedisSubscriber {
             Long partnerId = dto.getPartnerId();
 
             // 로그인 유저 채팅방 리스트 최신화 -> 로그인한 내 계정에 보냄
-            messagingTemplate.convertAndSend(
-                    "/sub/chat/roomList/" + userId, chatRoomListGetResponseList
-            );
+            for (ChatRoomGetResponse response : chatRoomListGetResponseList) {
+                messagingTemplate.convertAndSend(
+                        "/sub/chat/roomList/" + userId, response
+                );
+            }
 
             // 파트너 계정에도 리스트 최신화
 //            messagingTemplate.convertAndSend(
