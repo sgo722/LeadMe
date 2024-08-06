2f583374cf113b4aad704ea3f9f46ef2c917b2cd
jinwoo <zvzv9808@gmail.com>
2024-08-06 10:43:24 +0900

    fix: resolver 로직 수정

--- server/src/main/java/com/ssafy/withme/global/resolver/CurrentUserArgumentResolver.java
+++ server/src/main/java/com/ssafy/withme/global/resolver/CurrentUserArgumentResolver.java
@@ -6,6 +6,7 @@ import com.ssafy.withme.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
