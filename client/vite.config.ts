08b05f976914d588f34a951c25cb4a05a2dac46d
Jinwoo <zvzv9808@gmail.com>
2024-08-06 21:27:39 +0900

    fix: dockerfile 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: "0.0.0.0",
+  },
   plugins: [react()],
   // server: {
   //   host: "127.0.0.1", // 원하는 IP 주소
@@ -21,4 +25,5 @@ export default defineConfig({
       utils: path.resolve(__dirname, "./src/utils"),
     },
   },
+  assetsInclude: ["**/*.jpg"],
 });
