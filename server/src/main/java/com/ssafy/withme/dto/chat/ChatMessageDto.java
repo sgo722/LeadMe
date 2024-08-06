f934c5b93f336456eeb97c1c1efe9abe22d33a3a
jinwoo <zvzv9808@gmail.com>
2024-08-06 13:27:44 +0900

    fix: add Json Type Info

--- server/src/main/java/com/ssafy/withme/dto/chat/ChatMessageDto.java
+++ server/src/main/java/com/ssafy/withme/dto/chat/ChatMessageDto.java
@@ -1,5 +1,8 @@
 package com.ssafy.withme.dto.chat;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.ssafy.withme.domain.chat.ChatMessage;
 import com.ssafy.withme.domain.chat.constant.MessageStatus;
 import com.ssafy.withme.domain.chat.constant.MessageType;
@@ -12,8 +15,9 @@ import java.io.Serializable;
 @Builder
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public class ChatMessageDto implements Serializable {
 
     // 메시지 타입 : 입장, 채팅, 퇴장
@@ -43,4 +47,22 @@ public class ChatMessageDto implements Serializable {
                 .build();
     }
 
+    @JsonCreator
+    public ChatMessageDto(
+            @JsonProperty("type") MessageType type,
+            @JsonProperty("roomId") Long roomId,
+            @JsonProperty("userId") Long userId,
+            @JsonProperty("nickname") String nickname,
+            @JsonProperty("message") String message,
+            @JsonProperty("time") String time,
+            @JsonProperty("status") MessageStatus status
+    ){
+        this.type = type;
+        this.roomId = roomId;
+        this.userId = userId;
+        this.nickname = nickname;
+        this.message = message;
+        this.time = time;
+        this.status = status;
+    }
 }
--- server/src/main/java/com/ssafy/withme/dto/chat/MessageSubDto.java
+++ server/src/main/java/com/ssafy/withme/dto/chat/MessageSubDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.withme.dto.chat;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.ssafy.withme.dto.chat.response.ChatRoomGetResponse;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public class MessageSubDto implements Serializable {
     private Long userId;
     private Long partnerId;
