b806914b0640f79a1c42cc3a1a824009d6282e07
limjunhee <limjunhee4575@gmail.com>
2024-08-07 10:13:07 +0900

    flx: baseUrl 수정

--- client/src/axiosInstance/constants.ts
+++ client/src/axiosInstance/constants.ts
@@ -1,3 +1,3 @@
-export const baseUrl: string = "https://i11c109.p.ssafy.io:8090";
+export const baseUrl: string = "https://i11c109.p.ssafy.io";
 export const testUrl: string = "http://localhost:8090";
 export const sockUrl: string = "ws://i11c109.p.ssafy.io:8090";
