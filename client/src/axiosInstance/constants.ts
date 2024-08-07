01835163e3b5e8d794ac28b79ea1bafec20c26b8
jinwoo <zvzv9808@gmail.com>
2024-08-07 11:22:45 +0900

    fix: mongodb auth 설정 변경

--- client/src/axiosInstance/constants.ts
+++ client/src/axiosInstance/constants.ts
@@ -1,3 +1,4 @@
-export const baseUrl: string = "https://i11c109.p.ssafy.io";
+export const baseUrl: string = "http://i11c109.p.ssafy.io:8090";
 export const testUrl: string = "http://localhost:8090";
 export const sockUrl: string = "ws://i11c109.p.ssafy.io:8090";
+export const testSockUrl: string = "ws://localhost:8090";
