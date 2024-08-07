8c6e77431ca3c2941236230718e8f7462095e338
limjunhee <limjunhee4575@gmail.com>
2024-08-07 11:59:59 +0900

    fix: Edit BaseUrl

--- client/src/axiosInstance/constants.ts
+++ client/src/axiosInstance/constants.ts
@@ -1,4 +1,4 @@
-export const baseUrl: string = "http://i11c109.p.ssafy.io:8090";
+export const baseUrl: string = "http://i11c109.p.ssafy.io";
 export const testUrl: string = "http://localhost:8090";
 export const sockUrl: string = "ws://i11c109.p.ssafy.io:8090";
 export const testSockUrl: string = "ws://localhost:8090";
