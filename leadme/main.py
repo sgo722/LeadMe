4ef704c497fc5fd076753fb941ca91551447cab1
sgo722 <sgo722@naver.com>
2024-08-10 02:11:24 +0900

    fix : 파이썬 저장 경로 수정

--- leadme/main.py
+++ leadme/main.py
@@ -31,7 +31,7 @@ class Video(BaseModel):
     url : str
     youtubeId : str
 
-UPLOAD_DIRECTORY = "."
+# UPLOAD_DIRECTORY = "."
 TEMP_DIRECTORY = "video/temporary"
 PERMANENT_DIRECTORY_USER = "video/user"
 PERMANENT_DIRECTORY_CHALLENGE = "video/challenge"
@@ -39,11 +39,11 @@ PERMANENT_DIRECTORY_CHALLENGE_AUDIO = "video/challenge/audio"
 THUMBNAIL_DIRECTORY = "video/temporary/thumbnail"
 
 
-# TEMP_DIRECTORY = "/home/ubuntu/python/video/temporary"
-# PERMANENT_DIRECTORY_USER = "/home/ubuntu/python/video/user"
-# PERMANENT_DIRECTORY_CHALLENGE = "/home/ubuntu/python/video/challenge"
-# PERMANENT_DIRECTORY_CHALLENGE_AUDIO =  "/home/ubuntu/python/video/challenge/audio"
-# THUMBNAIL_DIRECTORY = "/home/ubuntu/python/video/temporary/thumbnail"
+TEMP_DIRECTORY = "/home/ubuntu/python/video/temporary"
+PERMANENT_DIRECTORY_USER = "/home/ubuntu/python/video/user"
+PERMANENT_DIRECTORY_CHALLENGE = "/home/ubuntu/python/video/challenge"
+PERMANENT_DIRECTORY_CHALLENGE_AUDIO =  "/home/ubuntu/python/video/challenge/audio"
+THUMBNAIL_DIRECTORY = "/home/ubuntu/python/video/temporary/thumbnail"
 
 
 ## 서비스 로직 호출 부분을 Ray로 병렬 처리한다.
