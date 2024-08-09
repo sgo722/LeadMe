8ac5cf5ca0c5fb26d1a6a97555588ad0644ec5f8
nbo5548 <nbo5548@naver.com>
2024-08-09 10:52:33 +0900

    fix: url 수정

--- server/src/main/java/com/ssafy/withme/controller/userchallengeLike/UserChallengeLikeController.java
+++ server/src/main/java/com/ssafy/withme/controller/userchallengeLike/UserChallengeLikeController.java
@@ -20,7 +20,7 @@ public class UserChallengeLikeController {
     private final TokenProvider tokenProvider;
 
     // 유저 게시글 좋아요 기능
-    @PostMapping("/like")
+    @PostMapping("/liked")
     public SuccessResponse<?> likeUserChallenge(
             @RequestHeader("Authorization") String authorization,
             @RequestParam("userChallengeId") Long userChallengeId
