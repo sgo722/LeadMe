40c114fdf889dc735b212014fab823623f33cfe3
nbo5548 <nbo5548@naver.com>
2024-08-12 17:15:23 +0900

    refactor: RankService 코드 리팩토링

--- server/src/main/java/com/ssafy/withme/service/rank/RankService.java
+++ server/src/main/java/com/ssafy/withme/service/rank/RankService.java
@@ -32,8 +32,6 @@ public class RankService {
         ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
 
         long itemsPerPage = 10;
-        long start = (pageNo - 1) * itemsPerPage;
-        long end = start + itemsPerPage - 1;
 
         // RDBMS에서 유저를 좋아요 순으로 가져오기
         Pageable pageable = PageRequest.of(pageNo.intValue() - 1, (int) itemsPerPage);
@@ -50,85 +48,5 @@ public class RankService {
                 ))
                 .collect(Collectors.toList());
 
-//        // 해당 페이지에 해당하는 상위 10명의 유저 정보 가져오기
-//        Set<ZSetOperations.TypedTuple<String>> typedTuples = zSetOperations.reverseRangeWithScores(key, start, end);
-//
-//        // Redis에 유저 정보 X
-//        if (typedTuples == null || typedTuples.isEmpty()) {
-//            log.warn("Redis에 데이터가 없어 RDBMS에서 정보를 가져옵니다.");
-//
-//            // RDBMS에서 유저를 좋아요 순으로 가져오기
-//            Pageable pageable = PageRequest.of(pageNo.intValue() - 1, (int) itemsPerPage);
-//
-//            List<RankResponseDto> rankResponseDtoList = userRepository.findTopUsersByLikes(pageable).stream()
-//                    .map(user -> new RankResponseDto(
-//                            user.getId(),
-//                            user.getNickname(),
-//                            user.getUserLikeCnt(),
-//                            (long) user.getToFollowList().size(),
-//                            user.getProfileImg()
-//                    ))
-//                    .collect(Collectors.toList());
-//
-//            // RDBMS -> Redis로 저장
-//            // Redis의 정보가 휘발될 경우를 대비
-//            rankResponseDtoList.forEach(rankResponseDto -> {
-//
-//                Long liked = rankResponseDto.getLiked();
-//
-//                if (liked == null)
-//                    liked = 0L;
-//
-//                zSetOperations.add(key, rankResponseDto.getUserNickname(), liked);
-//            });
-//
-//            return rankResponseDtoList;
-//        }
-//
-//        log.info("Redis에서 유저 정보 반환합니다!!");
-//        // Redis에 유저 정보 O
-//        // 유저 정보를 리스트로 변환
-//        return typedTuples.stream()
-//                .map(tuple -> {
-//                    String nickname = tuple.getValue();
-//                    Long score = tuple.getScore().longValue();
-//
-//                    return userRepository.findByNickname(nickname)
-//                            .map(user -> new RankResponseDto(
-//                                    user.getId(),
-//                                    nickname,
-//                                    score, // 좋아요 수
-//                                    (long) user.getToFollowList().size(), // 팔로워 수
-//                                    user.getProfileImg() // 프로필 이미지
-//                            ))
-//                            .orElse(null);
-//                })
-//                .filter(rankResponseDto -> rankResponseDto != null)
-//                .collect(Collectors.toList());
     }
-
-    // redis Ranking -> DB에 업데이트
-//    @Transactional
-//    public void updateLike() {
-//        String key = "user_likes";
-//        ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
-//
-//        // 페이지 번호에 따라 랭킹 출력
-//        Set<ZSetOperations.TypedTuple<String>> typedTuples = zSetOperations.reverseRangeWithScores(key, 0, -1);
-//
-//        if (typedTuples != null) {
-//            for (ZSetOperations.TypedTuple<String> tuple : typedTuples) {
-//                String nickname = tuple.getValue();
-//                Long score = tuple.getScore().longValue();
-//
-//                // Redis에서 가져온 닉네임에 해당하는 유저를 DB에서 찾아서 업데이트
-//                userRepository.findByNickname(nickname).ifPresent(user -> {
-//                    if (score != null) {
-//                        user.setUserLikeCnt(score);
-//                        userRepository.save(user); // RDBS에 업데이트
-//                    }
-//                });
-//            }
-//        }
-//    }
 }
