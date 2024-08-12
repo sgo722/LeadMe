5235911a4195c623431a800695a799291bcc0935
nbo5548 <nbo5548@naver.com>
2024-08-12 15:03:30 +0900

    refactor: User table userLikeCnt 제약조건 추가

--- server/src/main/java/com/ssafy/withme/domain/user/User.java
+++ server/src/main/java/com/ssafy/withme/domain/user/User.java
@@ -64,7 +64,8 @@ public class User extends BaseEntity implements UserDetails {
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<UserChallengeLike> userChallengeLikeList = new ArrayList<>();
 
-    private Long userLikeCnt;
+    @Column(name = "user_like_cnt", nullable = false)
+    private Long userLikeCnt = 0L;
 
     public User update(String name) {
         this.name = name;
