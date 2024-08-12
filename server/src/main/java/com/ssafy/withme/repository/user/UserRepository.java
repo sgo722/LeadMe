2b838851267e551d8eaad030b31a22a48b627b08
jinwoo <zvzv9808@gmail.com>
2024-08-12 15:55:37 +0900

    fix: fetch join 삭제

--- server/src/main/java/com/ssafy/withme/repository/user/UserRepository.java
+++ server/src/main/java/com/ssafy/withme/repository/user/UserRepository.java
@@ -23,10 +23,10 @@ public interface UserRepository extends JpaRepository<User,Long>, UserRepository
     List<User> findByNicknameContaining(String nickname);
 
 //    @Override
-    @Query("SELECT DISTINCT u FROM users u left join fetch u.toFollowList f WHERE u.nickname = :nickname")
+    @Query("SELECT DISTINCT u FROM users u left join u.toFollowList f on f.toUser.id=u.id WHERE u.nickname = :nickname")
     Optional<User> findByNickname(@Param("nickname") String nickname);
 
-    @Query("SELECT DISTINCT u FROM users u left join fetch u.toFollowList f ORDER BY u.userLikeCnt DESC")
+    @Query("SELECT DISTINCT u FROM users u left join u.toFollowList f on f.toUser.id=u.id ORDER BY u.userLikeCnt DESC")
 //    @Query("SELECT u FROM users u JOIN u.fromFollowList f GROUP BY u ORDER BY u.userLikeCnt DESC")
 //    @Override
     List<User> findTopUsersByLikes(Pageable pageable);
