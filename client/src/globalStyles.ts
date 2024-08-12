417bcb455ccb8b42d659a267c74fba6e2bb09cc8
limjunhee <limjunhee4575@gmail.com>
2024-08-12 17:21:26 +0900

    feat: 배틀결과 네온폰트 추가

--- client/src/globalStyles.ts
+++ client/src/globalStyles.ts
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'DOSIyagiMedium';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/DOSIyagiMedium.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
     body{
         color:${(props) => props.theme.colors.primary} // 기본 글자색 설정
     }
